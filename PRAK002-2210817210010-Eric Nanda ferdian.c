0f47419af1e4bcb075f6b39fbc000d0651f26060
ericnferdian <115312916+ericnferdian@users.noreply.github.com>
2022-10-08 06:31:09 +0800

    Add files via upload

---
+++ PRAK002-2210817210010-Eric Nanda ferdian.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Selamat Pagi,Eric Nanda Ferdian\n");
+    printf("Selamat Siang,Eric Nanda Ferdian\n");
+    printf("Selamat Malam,Eric Nanda Ferdian\n");
+}
---
+++ PRAK004-2210817210010-Eric Nanda ferdian.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#     Eric Nanda ferdian     #\n");
+    printf("#        2210817210010       #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+}
